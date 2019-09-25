f85939d7bf3fb9da620824c15a0f762588ce9616
Hicham Abdelatif <subzer0crack@gmail.com>
2019-09-25 13:14:50 +0100

    blog constructing : Add HomePage View Add blogurls templates dir base.html home.html

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from .models import BlogPost
 # Create your views here.
+
+class HomePage(ListView) :
+    queryset = BlogPost.objects.all()
+    context_object_name = "posts"
+    template_name = "home.html"
+
